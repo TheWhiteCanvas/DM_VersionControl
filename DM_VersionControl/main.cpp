db3dc10a08c1ce3cf2086b45111982d0f5c4297f
White Canvas <tfgwhitecanvas@gmail.com>
2024-09-27 03:35:32 -0300

    Added goodbye function.

--- DM_VersionControl/main.cpp
+++ DM_VersionControl/main.cpp
@@ -6,7 +6,13 @@ void greet()
 	std::cout << "Hello World! This is the Main Function. Welcome! \n\n\n";
 }
 
+void goodbye()
+{
+	std::cout << "Fare thee well. \n\n\n";
+}
+
 int main()
 {
 	greet();
+	goodbye();
 }
